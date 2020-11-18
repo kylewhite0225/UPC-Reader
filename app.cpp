ef178b3ef5068147364a6ddb86f9422fa6dec268
Kyle White <kylewhite0225@gmail.com>
2020-11-17 22:19:23 -0800

    Updated to handle user input and file not found exception.

--- app.cpp
+++ app.cpp
@@ -3,8 +3,9 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <time.h>
 #include <chrono>
+#include <regex>
+#include <exception>
 using namespace std;
 using namespace std::chrono;
 
@@ -17,12 +18,15 @@ BST<UPC> buildTree(ifstream *file) {
 	BST<UPC> tree;
 	string line;
 	while (getline(*file, line)) {
+		// Skip first line
 		if (line == "UPC,Description") {
 			continue;
 		}
 		stringstream ss(line);
 		string code;
 		string description;
+		// While loop alternates between increasing and decreasing count
+		// in order to switch between grabbing the UPC and the description.
 		int count = 0;
 		while (ss.good()) {
 			string temp;
@@ -44,16 +48,13 @@ BST<UPC> buildTree(ifstream *file) {
 }
 
 void performSearchBST(BST<UPC> tree, UPC key) {
-	clock_t t;
-	t = clock();
 	auto start = high_resolution_clock::now();
 	UPC found = tree.search(key);
 	if (found.getDescription() == "") {
 		cout << "Not found." << endl;
 	}
 	auto stop = high_resolution_clock::now();
-	auto duration = duration_cast<nanoseconds>(stop - start);
-	t = clock() - t;
+	auto duration = duration_cast<milliseconds>(stop - start);
 	cout << found.getDescription() << endl;
 	cout << "Lookup time: " << duration.count() << " milliseconds." << endl;
 }
@@ -61,21 +62,50 @@ void performSearchBST(BST<UPC> tree, UPC key) {
 int main() {
     //...
 	ifstream *file = new ifstream();
-    file->open("grocery_upc_database.csv");
+    bool fileValid = false;
+	string fileName;
+	do {
+		try {
+
+			cout << "Enter file name: ";
+			cin >> fileName;
+			file->open(fileName);
+			if (!file->is_open()) { // || !fileName.find(".csv")) {
+				throw out_of_range("Invalid file, or file not found.\n");
+			} else {
+				fileValid = true;
+			}
+		} catch (exception out_of_range) {
+			cout << "Invalid file, or file not found.\n" << endl;
+		}
+	} while (fileValid == false);
+	
 	BST<UPC> tree = buildTree(file);  //build binary search tree of UPC objects
 
 	string code;
-	cout << "Please enter a UPC code(! to quit): ";
-	cin >> code;
+	bool validUPC = false;
+	// Do-while loop uses a regex to verify user input matches the correct format of a 12 digit UPC.
+	do {
+		cout << "Please enter a UPC code(! to quit): ";
+		cin >> code;
+		validUPC = (code.length() == 12 && regex_match(code, regex("^[0-9]+$"))) || code == "!";
+		if (validUPC == false) {
+			cout << "UPC must be a 12 digit number." << endl;
+		}
+	} while (validUPC == false);
 	while (code != "!") {
-		
         long long entry = stoll(code); //convert user inputted string to type long long int
 		UPC key(entry);
 		performSearchBST(tree, key);	
-		
-		cout << "\nPlease enter a UPC code(! to quit): ";
-		cin >> code;
+		// Do-while loop uses a regex to verify user input matches the correct format of a 12 digit UPC.
+		do {
+			cout << "\nPlease enter a UPC code(! to quit): ";
+			cin >> code;
+			validUPC = (code.length() == 12 && regex_match(code, regex("^[0-9]+$"))) || code == "!";
+			if (validUPC == false) {
+				cout << "UPC must be a 12 digit number." << endl;
+			}
+		} while (validUPC == false);
 	}
-
 	return 0;
 } 
